b8b90d4c7e18f2b4ffe3eb7ef4558bd9c6a81c3a
wjc <351138151@qq.com>
2022-03-15 18:13:07 +0800

    master

---
+++ 1.js
@@ -0,0 +1,11 @@
+console.log("text0");
+console.log("text1");
+console.log("text2");
+console.log("text3");
+console.log("text4");
+console.log("text5");
+let a = 1;
+// if (a === 1) {
+//   console.log(a);
+// }
+ 
